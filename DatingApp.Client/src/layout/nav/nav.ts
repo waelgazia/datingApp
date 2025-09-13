ccb504f2a4d964a8bb5283ea056a1e6ae041274b
Wael Yassir Hashim <ya.4@hotmail.com>
2025-09-13 22:11:09 +0300

    Support themes.

--- DatingApp.Client/src/layout/nav/nav.ts
+++ DatingApp.Client/src/layout/nav/nav.ts
@@ -1,9 +1,8 @@
-import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
-import { AccountService } from '../../core/services/account-service';
+import { themes } from '../theme';
 import { LoginVM } from '../../interfaces/LoginVM';
 import { ToastService } from '../../core/services/toast-service';
 import { AccountService } from '../../core/services/account-service';
@@ -14,12 +13,20 @@ import { AccountService } from '../../core/services/account-service';
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
-export class Nav {
+export class Nav implements OnInit{
   private _router = inject(Router)
   private _toastService = inject(ToastService);
 
   protected accountService = inject(AccountService);
   protected loginVM = {} as LoginVM;
+
+  protected selectedTheme = signal<string>(localStorage.getItem('theme') || 'light');
+  protected themes : string[] = themes;
+
+  ngOnInit(): void {
+    // to set a theme using daisyUI
+    document.documentElement.setAttribute('data-theme', this.selectedTheme());
+  }
 
   login() {
     this.accountService.login(this.loginVM)
@@ -39,6 +46,20 @@ export class Nav {
   loggedOut() {
     this.accountService.logout();
     this._router.navigateByUrl('/');
+  }
+
+  onThemeSelected(theme: string) {
+    this.selectedTheme.set(theme);
+    localStorage.setItem('theme', theme);
+
+    // to set a theme using daisyUI
+    document.documentElement.setAttribute('data-theme', theme);
+
+    // to automatically close the dropdown after theme selection
+    const themeDropdown = document.activeElement as HTMLDivElement;
+    if (themeDropdown) {
+      themeDropdown.blur();
+    }
   }
 }
 
